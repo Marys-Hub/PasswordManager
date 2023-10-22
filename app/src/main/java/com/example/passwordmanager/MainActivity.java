c1422c771b79ec4462e5063612cf808572ad1999
MaryCaroline <marycaroline218@gmail.com>
2023-10-22 17:25:26 +0300

    commit 1 main activity with no usage

---
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.passwordmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.passwordmanager.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+    private Button buttonLogin;
+    private Button buttonRegister;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main); //new
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin); //new
+        buttonRegister =(Button) findViewById(R.id.buttonRegister);//new
+
+
+        //setSupportActionBar(binding.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the LoginActivity when the "LOG IN" button is clicked
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the RegisterActivity when the "Register" button is clicked
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+
+
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.buttonLogin)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
